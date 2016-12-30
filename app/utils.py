102b4dc1616f83038c5851da3f2c9dd83b8b2723
panasevychol <panasevychol@gmail.com>
2016-12-30 15:13:51 +0200

    A method to download game data improved + some smaller improvements

--- app/utils.py
+++ app/utils.py
@@ -5,6 +5,8 @@ import os
 from .constants import GAME_API_URL, PLATFORMS_KEYS
 
 
+GAMES_INFO = None
+
 def download_games_data(**get_options):
     url = GAME_API_URL
     for key,value in get_options.items():
@@ -14,6 +16,7 @@ def download_games_data(**get_options):
 
 def download_all_games_info():
     games_info = []
+    print('Downloading games info')
 
     for platform_key in PLATFORMS_KEYS:
         games_downloaded, games_total = 0, None
@@ -21,14 +24,17 @@ def download_all_games_info():
         while games_downloaded != games_total:
             games_data = download_games_data(platform=platform_key,
                                              offset=games_downloaded)
+            games_info += games_data['results']
 
-            for game in games_data['results']:
-                games_info.append({
-                    'name': game['name'],
-                    'original_release_date': game.get('original_release_date'),
-                    'platforms': [platform.get('abbreviation')
-                                  for platform in game.get('platforms', [])]
-                })
+            # Use the code below if you don't need to save all game info
+
+            # for game in games_data['results']:
+            #     games_info.append({
+            #         'name': game['name'],
+            #         'original_release_date': game.get('original_release_date'),
+            #         'platforms': [platform.get('abbreviation')
+            #                       for platform in game.get('platforms', [])]
+            #     })
 
             if not games_total:
                 games_total = games_data['number_of_total_results']
@@ -37,9 +43,8 @@ def download_all_games_info():
             print('Downoading %s %%' %
                   int((games_downloaded / float(games_total) * 100)))
 
+    print('Download complete')
     return games_info
-
-GAMES_INFO = None
 
 def get_all_games_info():
     if GAMES_INFO:
